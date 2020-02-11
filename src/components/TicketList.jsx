5eca5bfdb74fa2e0b74d82ea0670f21d18319fd3
Misaki Koonce and Will Quanstrom <misaki+will@>
2020-02-11 09:29:27 -0800

    Add loop for TicketList component.

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import Ticket from './Ticket';
 
+const masterTicketList = [
+  {
+    names: 'Thato and Haley',
+    location: '3A',
+    issue: `Firebase won't save record. Help!!!!!!`
+  },
+  {
+    names: 'Sleater and Kinney',
+    location: '4B',
+    issue: 'Fox image not displaying on page, can only see duck?'
+  },
+  {
+    names: 'Imani & Jacob',
+    location: '9F',
+    issue: 'Donkey picture not displaying on hover in Zoology app. :('
+  }
+]
+
 function TicketList(){
   return (
-    <Ticket
-      location="3A"
-      names="Thato and Haley"
-      issue="Firebase will not save record!"
-    />
+    <div>
+      <hr/>
+      {masterTicketList.map((ticket, index) =>
+        <Ticket
+          location={ticket.location}
+          names={ticket.names}
+          issue={ticket.issue}
+          key={index} />
+      )}
+    </div>
   );
 }
 
