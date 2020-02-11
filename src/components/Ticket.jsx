6afbad75b5cf7085ac21236832c2ea68cd04bd08
Misaki Koonce <misaki.koonce@gmail.com>
2020-02-10 17:27:12 -0800

    Reinstall react-hot-loader and fix bug.

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -4,8 +4,8 @@ import PropTypes from "prop-types";
 function Ticket(props){
   return (
     <div>
-        <h3>{props.location}- {props.names}</h3>
-        <p><em>{ProgressPlugin.issue}</em></p>
+        <h3>{props.location} - {props.names}</h3>
+        <p><em>{props.issue}</em></p>
         <hr/>
     </div>
   );
