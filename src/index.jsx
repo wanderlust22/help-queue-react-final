225fdf4ad4c03e73442d79d2fdaac071e05f3686
Misaki Koonce and Will Quanstrom <misaki+will@>
2020-02-11 08:52:17 -0800

    Install eslint and eslint loader.

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
 import { AppContainer } from 'react-hot-loader';
 
 const render = (Component) => {
@@ -14,8 +14,10 @@ const render = (Component) => {
 
 render(App);
 
+/*eslint-disable */
 if (module.hot) {
   module.hot.accept('./components/App', () => {
-    render(App)
+    render(App);
   });
 }
+/*eslint-enable */
