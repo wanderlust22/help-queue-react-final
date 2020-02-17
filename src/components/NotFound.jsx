16632cd00f31c038aa2a73ea89f35e7e299280c5
misakimichy <example@example.com>
2020-02-17 11:51:18 -0800

    Add props to 404 page.

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
-const NotFound = () => {
+const NotFound = props => {
     return(
         <div>
             <h1>404 Not Found</h1>
-            <p>The page you are looking for does not exist.</p>
+            <p>The page "{props.location.pathname}"" does not exist.</p>
             <Link to='/'>Home</Link>
         </div>
     )
+}
+
+NotFound.propTypes = {
+    location: PropTypes.object
 }
 
 export default NotFound
