c9c6712a66542ae1156848d4f4ff4883529bedbc
misakimichy <misaki.koonce@gmail.com>
2020-02-17 17:04:36 -0800

    Add style to NotFound component.

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -4,11 +4,30 @@ import PropTypes from 'prop-types'
 
 const NotFound = props => {
     return(
-        <div>
-            <h1>404 Not Found</h1>
-            <p>The page "{props.location.pathname}" does not exist.</p>
-            <Link to='/'>Home</Link>
-        </div>
+        <main className='not-found'>
+            <div className='container'>
+                <h1>404 Not Found</h1>
+                <p>The page "{props.location.pathname}" does not exist.</p>
+                <Link to='/'>Home</Link>
+            </div>
+            <style jsx>{`
+                .not-found {
+                    background: url('https://http.cat/404') no-repeat center center fixed;
+                    background-size: cover;
+                    height 100vh;
+                    overflow: hidden
+                }
+
+                .container {
+                    width: 80%;
+                    margin: 0 auto;
+                    padding: 1em;
+                    text-align: center;
+                    background-color: rgba(255, 255, 255, 0.3);
+                    border-radius: 4px;
+                }
+            `}</style>
+        </main>
     )
 }
 
